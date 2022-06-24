3ad9b6af186a91c7650c475e2f993aaa664b7ab0
Sandeep <3424988+sendy34@users.noreply.github.com>
2022-06-25 05:11:45 +0530

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -2,6 +2,8 @@ function Post() {
   return <h1>Page</h1>
 }
 
+export default Post
+
 export async function getStaticPaths() {
   /*
    *   If fallback is false, then any paths not returned by getStaticPaths will result in a 404 page.
