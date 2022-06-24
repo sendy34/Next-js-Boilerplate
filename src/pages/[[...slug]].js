9ccd6b41d1f1b6fd6a4fe8477daefe72b20f0b81
Sandeep <3424988+sendy34@users.noreply.github.com>
2022-06-25 05:10:16 +0530

    Create [[...slug]].js

---
+++ src/pages/[[...slug]].js
@@ -0,0 +1,21 @@
+function Post() {
+  return <h1>Page</h1>
+}
+
+export async function getStaticPaths() {
+  /*
+   *   If fallback is false, then any paths not returned by getStaticPaths will result in a 404 page.
+   *   This is useful if you know that all paths will be known at build time.
+   */
+  return {
+    paths: [{
+      params: { id: '1' },
+      params: { id: '2' }
+    }],
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  return {}
+}
