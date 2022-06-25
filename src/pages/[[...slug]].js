d546f15435cd3b7d26756d2b31681aaf616ad7e9
Sandeep <3424988+sendy34@users.noreply.github.com>
2022-06-25 11:49:39 +0530

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -11,8 +11,8 @@ export async function getStaticPaths() {
    */
   return {
     paths: [{
-      params: { id: '1' },
-      params: { id: '2' }
+      params: { slug: '1' },
+      params: { slug: '2' }
     }],
     fallback: false,
   }
